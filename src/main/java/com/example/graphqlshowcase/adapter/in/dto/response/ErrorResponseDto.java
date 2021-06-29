6a448a51c1363a8010ba090f7997947e06896145
Vladimir.Conev <conev.vladimir@gmail.com>
2021-06-29 13:52:57 +0200

    minor changes within error response DTO

--- src/main/java/com/example/graphqlshowcase/adapter/in/dto/response/ErrorResponseDto.java
+++ src/main/java/com/example/graphqlshowcase/adapter/in/dto/response/ErrorResponseDto.java
@@ -10,30 +10,23 @@ import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponseDto implements Serializable {
-	
 	
 	private static final long serialVersionUID = -1812607289914319079L;
 
-	@JsonInclude(JsonInclude.Include.NON_NULL)
     private String code;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String status;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String httpMethod;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String exception;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String path;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String message;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonFormat(pattern = "yyyy-MM-ddTHH:mm:SS.ssZ")
     private String timestamp;
 
