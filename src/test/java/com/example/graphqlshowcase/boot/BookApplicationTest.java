baead0b9e718f3ce4a625165ee13a4e92c1cc3f3
SCALEFOCUS\vladimir.conev <conev.vladimir@gmail.com>
2023-10-09 12:58:05 +0200

    use mockMVC in tests

--- src/test/java/com/example/graphqlshowcase/boot/BookApplicationTest.java
+++ src/test/java/com/example/graphqlshowcase/boot/BookApplicationTest.java
@@ -2,28 +2,35 @@ package com.example.graphqlshowcase.boot;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.graphqlshowcase.adapter.in.BookRestController;
 import com.example.graphqlshowcase.adapter.in.GraphQLBookRestController;
 import com.example.graphqlshowcase.adapter.in.dto.response.BookResponse;
 import com.example.graphqlshowcase.domain.valueobject.Genre;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers(disabledWithoutDocker = true)
+@AutoConfigureMockMvc
 class BookApplicationTest {
 
   static final MongoDBContainer mongoDBContainer =
@@ -38,6 +45,9 @@ class BookApplicationTest {
   @Autowired private BookRestController bookRestController;
 
   @Autowired private GraphQLBookRestController graphQLBookRestController;
+
+  @Autowired private MockMvc mockMvc;
+  @Autowired private ObjectMapper objectMapper;
 
   @BeforeAll
   static void setUp() {
@@ -62,43 +72,67 @@ class BookApplicationTest {
   }
 
   @Test
-  void createBookAndRetrieveById() {
+  void createBook() throws Exception {
     var createBookRequest = TestObjects.createBookRequest();
-    var responseEntity = bookRestController.createBook(createBookRequest);
+    var mvcResult =
+        mockMvc
+            .perform(
+                post("/books")
+                    .content(objectMapper.writeValueAsString(createBookRequest))
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andReturn();
+    var response = mvcResult.getResponse();
+    assertThat(response).isNotNull();
+    var bookResponse = objectMapper.readValue(response.getContentAsString(), BookResponse.class);
+    assertThat(bookResponse).isNotNull();
 
-    assertNotNull(responseEntity);
-    assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-    assertNotNull(responseEntity.getBody());
-    assertThat(responseEntity.getBody()).isInstanceOf(BookResponse.class);
-    var bookResponse = responseEntity.getBody();
-    assertNotNull(bookResponse.id());
-    assertEquals(bookResponse.isbn(), createBookRequest.isbn());
-    assertEquals(bookResponse.genre(), createBookRequest.genre());
+    mvcResult =
+        mockMvc
+            .perform(
+                get("/books/%s".formatted(bookResponse.id()))
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
 
-    var bookByIdResponseEntity = bookRestController.retrieveBookById(bookResponse.id());
-    assertEquals(HttpStatus.OK, bookByIdResponseEntity.getStatusCode());
-    assertEquals(responseEntity.getBody(), bookByIdResponseEntity.getBody());
+    var bookResponseOnGet =
+        objectMapper.readValue(mvcResult.getResponse().getContentAsString(), BookResponse.class);
+    assertThat(bookResponseOnGet).isNotNull();
+    assertEquals(bookResponse.id(), bookResponseOnGet.id());
+    assertEquals(bookResponse.title(), bookResponseOnGet.title());
+    assertEquals(bookResponse.genre(), bookResponseOnGet.genre());
   }
 
   @Test
-  void updateBook() {
+  void updateBook() throws Exception {
     var createBookRequest = TestObjects.createBookRequest();
-    var responseEntity = bookRestController.createBook(createBookRequest);
+    var mvcResult =
+        mockMvc
+            .perform(
+                post("/books")
+                    .content(objectMapper.writeValueAsString(createBookRequest))
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andReturn();
+    var response = mvcResult.getResponse();
+    assertThat(response).isNotNull();
+    var bookResponse = objectMapper.readValue(response.getContentAsString(), BookResponse.class);
+    assertThat(bookResponse).isNotNull();
 
-    assertNotNull(responseEntity);
-    assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-    assertNotNull(responseEntity.getBody());
-
-    var bookResponse = responseEntity.getBody();
     var bookId = bookResponse.id();
     var bookUpdateRequest =
         TestObjects.updateBookRequestByTitleAndGenre("TEST_UPDATE", Genre.FANTASY.name());
+    mvcResult =
+        mockMvc
+            .perform(
+                put("/books/%s".formatted(bookId))
+                    .content(objectMapper.writeValueAsString(bookUpdateRequest))
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    var bookResponseUponUpdate =
+        objectMapper.readValue(mvcResult.getResponse().getContentAsString(), BookResponse.class);
 
-    var bookResponseResponseEntity = bookRestController.updateBook(bookUpdateRequest, bookId);
-    assertEquals(HttpStatus.OK, bookResponseResponseEntity.getStatusCode());
-    var bookResponseUponUpdate = bookResponseResponseEntity.getBody();
-
-    assert bookResponseUponUpdate != null;
     assertNotNull(bookResponseUponUpdate.title());
     assertEquals(bookUpdateRequest.title(), bookResponseUponUpdate.title());
     assertNotNull(bookResponseUponUpdate.genre());
